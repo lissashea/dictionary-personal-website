a2266e8babf3d826c9f0b96b43d5dfcdeae64dd5
“Lissa <lwarshaw674@gmail.com>
2023-03-08 09:25:37 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// get references to book cover and button elements
+const bookCover = document.querySelector('.book-cover-container');
+const openBookBtn = document.querySelector('#open-book-btn');
+
+// get reference to main content element
+const mainContent = document.querySelector('.main-content');
+
+// add click event listener to button
+openBookBtn.addEventListener('click', function() {
+  // hide book cover and show main content
+  bookCover.style.display = 'none';
+  mainContent.style.display = 'grid';
+});
