ba0b90cee809e3215f1bae996f6a29fddc68ed0a
“Lissa <lwarshaw674@gmail.com>
2023-03-09 12:33:38 -0500

    updated w3 image

--- main.js
+++ main.js
@@ -15,42 +15,10 @@ openBookBtn.addEventListener('click', function() {
 // get references to sound byte and button elements
 const pronounceButton = document.querySelector('#pronounce');
 const audio = document.querySelector('#audio');
-
+// add click event listener to button
 pronounceButton.addEventListener('click', () => {
   audio.currentTime = 0;
   audio.play();
 });
 
-// var responsiveSlider = function() {
-
-//   var slider = document.getElementById("slider");
-//   var sliderWidth = slider.offsetWidth;
-//   var slideList = document.getElementById("slideWrap");
-//   var count = 1;
-//   var items = slideList.querySelectorAll("li").length;
-  
-//   window.addEventListener('resize', function() {
-//     sliderWidth = slider.offsetWidth;
-//   });
-  
-//   var nextSlide = function() {
-//     if(count < items) {
-//       slideList.style.left = "-" + count * sliderWidth + "px";
-//       count++;
-//     }
-//     else if(count = items) {
-//       slideList.style.left = "0px";
-//       count = 1;
-//     }
-//   };
-  
-//   setInterval(function() {
-//     nextSlide()
-//   }, 5000);
-  
-//   };
-  
-//   window.onload = function() {
-//   responsiveSlider();
-//   }
   
