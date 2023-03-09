229ddd81205b13c96e52ca274ad48c1488effb4a
“Lissa <lwarshaw674@gmail.com>
2023-03-09 15:48:21 -0500

    script stuff

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+// // get references to book cover and button elements
+// const bookCover = document.querySelector('.book-cover-container');
+// const openBookBtn = document.querySelector('#open-book-btn');
+
+// // get reference to main content element aka the body of the site
+// const mainContent = document.querySelector('.main-content');
+
+// // add click event listener to button
+// openBookBtn.addEventListener('click', function() {
+//   // hide book cover and show main content
+//   bookCover.style.display = 'none';
+//   mainContent.style.display = 'grid';
+// });
+
+// // get references to sound byte and button elements
+// const pronounceButton = document.querySelector('#pronounce');
+// const audio = document.querySelector('#audio');
+// // add click event listener to button
+// pronounceButton.addEventListener('click', () => {
+//   audio.currentTime = 0; /* setting currentTime to 0 that the audio file starts playing from the beginning even if it has already been played before.*/
+//   audio.play();
+// });
+
 // get references to book cover and button elements
 const bookCover = document.querySelector('.book-cover-container');
 const openBookBtn = document.querySelector('#open-book-btn');
@@ -21,5 +44,3 @@ pronounceButton.addEventListener('click', () => {
   audio.currentTime = 0; /* setting currentTime to 0 ensures that the audio file starts playing from the beginning even if it has already been played before.*/
   audio.play();
 });
-
-  
