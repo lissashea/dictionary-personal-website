0d0dced11245ca43266d4b9246f252494ad32aad
“Lissa <lwarshaw674@gmail.com>
2023-03-09 13:21:52 -0500

    update grid to flex for media

--- main.js
+++ main.js
@@ -2,13 +2,14 @@
 const bookCover = document.querySelector('.book-cover-container');
 const openBookBtn = document.querySelector('#open-book-btn');
 
-// get reference to main content element
+// get reference to main content element aka the body of the site
 const mainContent = document.querySelector('.main-content');
 
 // add click event listener to button
 openBookBtn.addEventListener('click', function() {
   // hide book cover and show main content
   bookCover.style.display = 'none';
+  /* by setting the display property to 'grid', the element is displayed using the CSS grid layout. */
   mainContent.style.display = 'grid';
 });
 
@@ -17,7 +18,7 @@ const pronounceButton = document.querySelector('#pronounce');
 const audio = document.querySelector('#audio');
 // add click event listener to button
 pronounceButton.addEventListener('click', () => {
-  audio.currentTime = 0;
+  audio.currentTime = 0; /* setting currentTime to 0 ensures that the audio file starts playing from the beginning even if it has already been played before.*/
   audio.play();
 });
 
